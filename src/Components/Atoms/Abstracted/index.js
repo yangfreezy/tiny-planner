d21d1fb9ac44cc9d34b71124c5aee406e2ee4a5b
yang <yueyangyu42@gmail.com>
2020-04-06 19:33:39 -0700

    Delete unused components

--- src/Components/Atoms/Abstracted/index.js
+++ src/Components/Atoms/Abstracted/index.js
@@ -1,7 +1,6 @@
-import { Icon } from "./Icon/Icon";
 import { LinkWrapper } from "./LinkWrapper/LinkWrapper";
 import { PrimaryButton } from "./PrimaryButton/PrimaryButton";
 import { Select } from "./Select/Select";
 import { Text } from "./Text/Text";
 
-export { Icon, LinkWrapper, PrimaryButton, Select, Text };
+export { LinkWrapper, PrimaryButton, Select, Text };
--- src/Components/Organisms/index.js
+++ src/Components/Organisms/index.js
@@ -1,15 +1,7 @@
-import { DailyMealsList } from "./DailyMealsList/DailyMealsList";
 import { NavBar } from "./NavBar/NavBar";
 import { RecipeList } from "./RecipeList/RecipeList";
 import { SavedRecipeList } from "./SavedRecipeList/SavedRecipeList";
 import { SearchForm } from "./SearchForm/SearchForm";
 import { WeeklyMealPlanList } from "./WeeklyMealPlanList/WeeklyMealPlanList";
 
-export {
-  DailyMealsList,
-  NavBar,
-  RecipeList,
-  SavedRecipeList,
-  SearchForm,
-  WeeklyMealPlanList
-};
+export { NavBar, RecipeList, SavedRecipeList, SearchForm, WeeklyMealPlanList };
